3be17b2167075e1c1bd197fe330e10be63e011eb
csonnic03 <53355831+csonnic03@users.noreply.github.com>
2021-01-07 15:43:14 -0700

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -53,3 +53,25 @@ function showSlides() {
     });
   }, false);
 })();
+
+/*Order Form*/
+function updateTotal(){
+  var tot = 0;
+  tot += f.tiger.value * 3 +
+         f.monkey.value * 3.5 +
+         f.elephant.value * 12 +
+         f.penguin.value * 12;
+  document.getElementById("cost").value = tot.toFixed(2);
+}
+
+var f = document.forms['order-form'];
+var sel = document.getElementsByTagName("select");
+
+for(var i=0; i<sel.length; i++) {
+  sel[i].onchange = function(){updateTotal()};
+}
+
+f.onsubmit = function(){
+  alert("Your total cost will be: \\$" + f.cost.value);
+  return false;
+}
